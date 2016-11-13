fa894c506300174f59c37b26ddb6405e1d521a9b
imccausl <imccausl@icloud.com>
2016-11-13 08:21:59 -0500

    completed

--- destroy.js
+++ destroy.js
@@ -1,20 +1,23 @@
 function destroyer(arr) {
 	var filtered = [];
 	var args = [];
-	
+		
 	for (var i=1; i<arguments.length; i++) {
 		args.push(arguments[i]);
 	}
-	
-	console.log(args);
-	filtered = arr.filter(function(element) {
-		return args.forEach(function(el) {
-			return element === el;
-		});
+
+	filtered = arr.filter(function(el) {
+		if (args.indexOf(el) === -1) {
+			return true;
+		}
+		
+		return false;
 	});
-	
+		
 	console.log(filtered);
 	return arr;
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+destroyer(["tree", "hamburger", 53], "tree", 53);
+destroyer([3, 5, 1, 2, 2], 2, 3, 5);
